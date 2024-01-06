2a83945b310f487c2fef038344b4347d71d8ee8a
xoboom <tdub.walton3@gmail.com>
2024-01-06 12:31:48 -0800

    blog

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.static('public')); // Assuming your HTML/CSS/JS files are in a directory named 'public'
 
 app.listen(port, () => {
